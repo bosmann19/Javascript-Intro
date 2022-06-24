9637a9e4b2bda321b5f67d1da452ee147484c902
Olatunbosun Akapo <olatunbosun.akapo@gmail.com>
2022-06-24 11:49:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var myName = "Akapo Olatunbosun";
+      console.log("My name is " + myName);
+      alert("my name is " + myName);
+var height = "5'8";
+      console.log("I am " + height);
+      alert("I am " + height);
+var country = "Nigeria";
+      console.log("The name of my country is " + country);
+      alert("The name of my country is " + country);
